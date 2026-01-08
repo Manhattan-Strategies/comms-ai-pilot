ba9190d56fa6c761715093fcf6a7739bb191a72b
Manhattan Strategies <jnguyen@manhattanstrategies.com>
2026-01-08 17:03:23 -0500

    add landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,20 @@
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
 import HeroSection from "@/components/home/hero-section";
+import DemoSection from "@/components/home/demo-section";
+import HowItWorksSection from "@/components/home/how-it-works-section";
+import PricingSection from "@/components/home/pricing-section";
+import CTASection from "@/components/home/cta-section";
 export default function Home() {
   return (
-    <div className="">
+    <div className="relative w-full">
       {/* <h1 className="text-4xl font-bold animate-pulse">Manhattan AI Pilot</h1>
       <Button variant="outline" size="lg">Click me</Button> */}
       <HeroSection />
-      {/* <DemoSection/> */}
-      {/* <HotItWorksSection/> */}
-      {/* <PricingSection/> */}
-      {/* <CTASection/> */}
+      <DemoSection />
+      <HowItWorksSection />
+      <PricingSection />
+      <CTASection />
     </div>
   );
 }
--- components/common/footer.tsx
+++ components/common/footer.tsx
@@ -1,7 +1,3 @@
 export default function Footer() {
-  return (
-    <footer className="bg-gray-50 py-12">
-      <p>Footer</p>
-    </footer>
-  );
+  return <footer className="bg-gray-50 py-12">Footer</footer>;
 }
